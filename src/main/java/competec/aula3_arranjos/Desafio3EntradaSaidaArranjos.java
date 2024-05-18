4193e0ce8ae0b4d2bf66797e33e88f85da9191f7
memelx <166915314+memelx@users.noreply.github.com>
2024-05-18 10:38:30 -0300

    Update Desafio3EntradaSaidaArranjos.java

--- src/main/java/competec/aula3_arranjos/Desafio3EntradaSaidaArranjos.java
+++ src/main/java/competec/aula3_arranjos/Desafio3EntradaSaidaArranjos.java
@@ -14,7 +14,12 @@ public class Desafio3EntradaSaidaArranjos {
 		 * Desafio extra:
 		 * Crie outro arranjo e inverta a ordem dos valores do primeiro arranjo.
 		 */
+		int numeros [] = {10;32;50};
+	
+	} int soma =numeros{0} + numeros{1} + numeros{3};
+System.out.println(soma);
+	
 
-	}
+
 
 }
